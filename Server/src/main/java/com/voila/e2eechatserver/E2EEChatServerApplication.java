cf385ce8b0d871a5473b773374d826347276d530
voila <voila1106@gmail.com>
2024-11-21 21:42:31 +0800

    统一数据表初始化位置

--- Server/src/main/java/com/voila/e2eechatserver/E2EEChatServerApplication.java
+++ Server/src/main/java/com/voila/e2eechatserver/E2EEChatServerApplication.java
@@ -1,31 +1,11 @@
 package com.voila.e2eechatserver;
 
-import com.voila.e2eechatserver.mapper.AccountMapper;
-import com.voila.e2eechatserver.mapper.UserConfigMapper;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class E2EEChatServerApplication {
-
-	AccountMapper accountMapper;
-	UserConfigMapper userConfigMapper;
-
-	@Autowired
-	public E2EEChatServerApplication(AccountMapper accountMapper, UserConfigMapper userConfigMapper) {
-		this.accountMapper = accountMapper;
-		this.userConfigMapper = userConfigMapper;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(E2EEChatServerApplication.class, args);
-	}
-
-	@PostConstruct
-	public void init() {
-		accountMapper.init();
-		userConfigMapper.init();
 	}
 }
--- Server/src/main/java/com/voila/e2eechatserver/common/ComponentManager.java
+++ Server/src/main/java/com/voila/e2eechatserver/common/ComponentManager.java
@@ -2,6 +2,7 @@ package com.voila.e2eechatserver.common;
 
 import com.voila.e2eechatserver.mapper.AccountMapper;
 import com.voila.e2eechatserver.mapper.MessageQueueMapper;
+import com.voila.e2eechatserver.mapper.UserConfigMapper;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,14 +13,22 @@ public class ComponentManager {
     private AccountMapper am;
     @Autowired
     private MessageQueueMapper mqm;
+    @Autowired
+    private UserConfigMapper ucm;
 
     public static AccountMapper accountMapper;
     public static MessageQueueMapper messageQueueMapper;
+    public static UserConfigMapper userConfigMapper;
 
     @PostConstruct
     public void init() {
         accountMapper = am;
         messageQueueMapper = mqm;
+        userConfigMapper = ucm;
+
+        am.init();
+        mqm.init();
+        ucm.init();
     }
 
 }
